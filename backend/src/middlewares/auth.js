eb13cf42be5e1c756c9e140eb110cb1397b901a8
Abdullah Ahmed <abdullah.ahmed.pisces.1010@gmail.com>
2025-07-19 14:37:33 +0530

    updated authentication

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,7 +3,7 @@ import User from "../models/User.js"
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header("Authorization")?.replace("Bearer", "")
+        const token = req.header("Authorization")?.replace("Bearer", "").trim()
 
         if(!token) {
             return res.status(401).json({
